c17460a7c3920069500d16c6e58d44315d369a5e
Iyed523 <iyedloghmari@hotmail.com>
2025-11-20 19:38:28 +0100

    Initial commit: Architecture complète avec Pivot et Wazuh

---
+++ target-samba/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 1. Démarrer l'agent Wazuh (La surveillance)
+echo "--> Démarrage de Wazuh Agent..."
+service wazuh-agent start
+
+# 2. Démarrer Samba (Le service vulnérable)
+echo "--> Démarrage de Samba..."
+service nmbd start
+service smbd start
+
+# 3. Boucle infinie pour garder le conteneur vivant
+# On affiche les logs de Wazuh en continu, c'est pratique pour le debug
+echo "--> Conteneur prêt. Surveillance des logs..."
+tail -f /var/ossec/logs/ossec.log
